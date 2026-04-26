a6370b5043ba145f6388944a200cbdb51dd01bdb
arun•°Cumar <arunkumar773681@gmail.com>
2026-04-26 15:58:21 +0530

    Update ping.js
    Signed-off-by: arun•°Cumar  <arunkumar773681@gmail.com>

--- plugins/system/ping.js
+++ plugins/system/ping.js
@@ -18,17 +18,10 @@ export default async (sock, msg, args) => {
         await sock.sendMessage(chat, { text: "🚀 Connecting to NEXA-BOT Server..." });
 
         const pingValue = Math.abs(Date.now() - (msg.messageTimestamp * 1000));
-        const speedStatus = pingValue < 500 ? "Turbo 🚀" : "Normal ⚡";
-        const netStatus = "🟢 High Speed";
 
-        const pingMsg = getRandomPing(pingValue, speedStatus, netStatus);
+        const pingMsg = getRandomPing(pingValue);
 
         if (fs.existsSync(imagePath)) {
-            await sock.sendMessage(chat, {
-                image: fs.readFileSync(imagePath),
-                caption: pingMsg
-            }, { quoted: msg });
-        } else {
             await sock.sendMessage(chat, {
                 text: pingMsg
             }, { quoted: msg });

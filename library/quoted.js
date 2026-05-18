b513e8809be8b9168fc20cbd980cb63e9ea58585
arakbox3 <115354476+arakbox3@users.noreply.github.com>
2026-05-18 12:49:42 +0530

    Update quoted.js
    Signed-off-by: arakbox3 <115354476+arakbox3@users.noreply.github.com>

--- library/quoted.js
+++ library/quoted.js
@@ -1,7 +1,11 @@
 // © 2026 arun•°Cumar. All Rights Reserved.
 
 import fs from 'fs';
+import { fileURLToPath } from 'url';
 
+const __filename = fileURLToPath(import.meta.url);
+const cacheBuster = `?update=${Date.now()}`;
+    
 const fquoted = {
     channel: {
         key: {
@@ -21,4 +25,19 @@ const fquoted = {
 };
 
 export default fquoted;
+
+ //watching 
+fs.watchFile(__filename, async () => {
+    // temporary stop
+    fs.unwatchFile(__filename);
+    
+    console.log('\x1b[0;32m' + __filename + ' \x1b[1;32mupdated!\x1b[0m');
+    
+    try {
+        await import(`./${path.basename(__filename)}${cacheBuster}`);
+    } catch (error) {
+        console.error('\x1b[1;31mError re-loading file:\x1b[0m', error);
+    }
+});
+
 

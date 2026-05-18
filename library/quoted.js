31662866b5ab7ab7ae42334dfb3905606fb131f6
arakbox3 <115354476+arakbox3@users.noreply.github.com>
2026-05-18 12:40:09 +0530

    Create quoted.js
    Signed-off-by: arakbox3 <115354476+arakbox3@users.noreply.github.com>

---
+++ library/quoted.js
@@ -0,0 +1,24 @@
+// © 2026 arun•°Cumar. All Rights Reserved.
+
+import fs from 'fs';
+
+const fquoted = {
+    channel: {
+        key: {
+            fromMe: false,
+            participant: "0@s.whatsapp.net",
+            remoteJid: "27796262030@s.whatsapp.net"
+        },
+        message: {
+            newsletterAdminInviteMessage: {
+                newsletterJid: "0@newsletter",
+                newsletterName: "arun•°Cumar",
+                caption: " BAT-MAN MD",
+                inviteExpiration: "0"
+            }
+        }
+    }
+};
+
+export default fquoted;
+

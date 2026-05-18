95249f88b2e2bb8b5f025f527f783b85765d3a3b
arakbox3 <115354476+arakbox3@users.noreply.github.com>
2026-05-18 13:15:11 +0530

    Create upload.js
    Signed-off-by: arakbox3 <115354476+arakbox3@users.noreply.github.com>

---
+++ library/upload.js
@@ -0,0 +1,25 @@
+import FormData from "form-data";
+import fetch "node-fetch";
+
+async function tempfiles(buffer, filename = "BAT-MAN.jpg") {
+    const buf = Buffer.isBuffer(buffer) ? buffer : Buffer.from(buffer);
+    const form = new FormData();
+    form.append("file", buf, { 
+        filename: filename, 
+        contentType: "image/jpeg"
+    });
+    
+    const res = await fetch("https://tmpfiles.org/api/v1/upload", {
+      method: "POST",
+      body: form,
+      headers: form.getHeaders()
+    });
+    
+    const json = await res.json();
+    const idMatch = json.data.url.match(/\/(\d+)\//);
+    const id = idMatch[1];
+    return `https://tmpfiles.org/dl/${id}/${filename}`;
+}
+
+exports default tempfiles;
+

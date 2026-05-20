2e3f97e9c27bf2a02893cff5f98acbfc04f9e7ba
arun•°Cumar <arunkumar773681@gmail.com>
2026-05-20 11:45:03 +0530

    Create ping.js
    Signed-off-by: arun•°Cumar  <arunkumar773681@gmail.com>

---
+++ plugins/ping.js
@@ -0,0 +1,36 @@
+   const ping = {
+    command: 'ping',
+    description: 'Check bot response time',
+    category: 'general',
+    execute: async (sock, m, {
+        args,
+        text,
+        q,
+        quoted,
+        mime,
+        qmsg,
+        isMedia,
+        groupMetadata,
+        groupName,
+        participants,
+        groupOwner,
+        groupAdmins,
+        isBotAdmins,
+        isAdmins,
+        isGroupOwner,
+        isCreator,
+        prefix,
+        reply,
+        config
+    }) => {
+        const start = Date.now();
+        await reply('📡 Pong!');
+        const latency = Date.now() - start;
+        
+         await reply(`📡 Pong!\n⏱️
+          Response Time: ${latency}ms\n💭
+          Runtime: ${process.uptime().toFixed(2)}s`);
+    }
+};
+
+export default ping;

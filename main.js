d7c6803cde95ce110ccb8be34e607368b6f2b06f
arun•°Cumar <arunkumar773681@gmail.com>
2026-05-26 12:22:46 +0530

    Update main.js
    Signed-off-by: arun•°Cumar  <arunkumar773681@gmail.com>

--- main.js
+++ main.js
@@ -103,7 +103,7 @@ const clientstart = async() => {
         }
     };
     
-    const { state, saveCreds } = await useMultiFileAuthState(`./${config.session}, session`);
+    const { state, saveCreds } = await useMultiFileAuthState(`./${config.session}`);
     const { version, isLatest } = await fetchLatestBaileysVersion();
     
    const sock = makeWASocket({
